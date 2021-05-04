0c95b1c4aa4e008e4d9514dad17de012fadcb101
Jie <1584648244@qq.com>
2021-05-04 22:39:11 +0800

    Update test of examples.

--- test/test_examples.py
+++ test/test_examples.py
@@ -1,22 +1,22 @@
 from run_this import evaluate
 
 def test_reno():
-    block_traces = ["datasets/application_traces/data_video.csv", "datasets/application_traces/data_audio.csv"]
-    network_trace = "datasets/network_traces/trace.txt"
+    block_traces = ["datasets/scenario_2/blocks/block_video.csv", "datasets/scenario_2/blocks/block_audio.csv"]
+    network_trace = "datasets/scenario_2/networks/traces_7.txt"
     log_packet_file = "output/packet_log/packet-0.log"
     solution_file = 'solution_demos.reno.solution'
     evaluate(solution_file, block_traces, network_trace, log_packet_file)
 
 def test_rl_torch():
-    block_traces = ["datasets/application_traces/data_video.csv", "datasets/application_traces/data_audio.csv"]
-    network_trace = "datasets/network_traces/trace.txt"
+    block_traces = ["datasets/scenario_2/blocks/block_video.csv", "datasets/scenario_2/blocks/block_audio.csv"]
+    network_trace = "datasets/scenario_2/networks/traces_7.txt"
     log_packet_file = "output/packet_log/packet-0.log"
     solution_file = 'solution_demos.rl_torch.solution'
     evaluate(solution_file, block_traces, network_trace, log_packet_file)
 
 def test_rl_tensorflow():
-    block_traces = ["datasets/application_traces/data_video.csv", "datasets/application_traces/data_audio.csv"]
-    network_trace = "datasets/network_traces/trace.txt"
+    block_traces = ["datasets/scenario_2/blocks/block_video.csv", "datasets/scenario_2/blocks/block_audio.csv"]
+    network_trace = "datasets/scenario_2/networks/traces_7.txt"
     log_packet_file = "output/packet_log/packet-0.log"
     solution_file = 'solution_demos.rl_tensorflow.solution'
     evaluate(solution_file, block_traces, network_trace, log_packet_file)
